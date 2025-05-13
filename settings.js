16d203a518beb005e8d1b999175360a5463f2120
Ohidurr <ohidurrahman96@gmail.com>
2025-05-12 21:44:58 -0400

    add rss

--- settings.js
+++ settings.js
@@ -1,12 +1,20 @@
 module.exports = {
-    TEST_MODE: false,
-    DRY_RUN: false,
-    LIMIT_TWEETS: 5,
-    keywords: [
-      'code', 'redeem', 'primogem', 'stellar jade', 'gift', 'reward',
-      'login', 'free', 'primo', 'gems','Redemption','Codes','livestream','jades','hsr',
-      'test'
-    ],
-    accounts: ['ittosminimelon','GenshinImpact','Gacha_Zone','WahidXD']
-    //['GenshinImpact', 'honkaistarrail', 'MarvelRivals','ZZZ_EN']
-  };
+  TEST_MODE: false,
+  DRY_RUN: false,
+  LIMIT_TWEETS: 5,
+  keywords: [
+    'code', 'redeem', 'primogem', 'stellar jade', 'gift', 'reward',
+    'login', 'free', 'primo', 'gems','Redemption','Codes','livestream','jades','hsr',
+    'test'
+  ],
+  accounts: ['Gacha_Zone'],
+  //['ittosminimelon','GenshinImpact','Gacha_Zone','WahidXD']
+  //['GenshinImpact', 'honkaistarrail', 'MarvelRivals','ZZZ_EN']
+
+  rssFeeds: [
+    {
+      name: 'Gacha_Zone',
+      url: 'https://xcancel.com/Gacha_Zone/rss'
+    }
+  ]
+};
